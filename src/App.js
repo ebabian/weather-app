6d9070b6047ff6f9704e8178acd8af1466199be1
ebabian <babianelektra@gmail.com>
2021-02-07 19:01:53 -0500

    changed to openweather api

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ function App() {
 
   const [data, setData] = useState([])
   const [isLoading, setLoading] = useState(true)
-  const url = 'http://api.weatherapi.com/v1/forecast.json'
 
   const API_KEY = process.env.REACT_APP_API_KEY;
 
+  const url = `https://api.openweathermap.org/data/2.5/`
+
   const params = {
       key: API_KEY,
-      q: 'New York',
-      days: 5
+      zipcode: '11238'
   }
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const asyncRes = await axios(url, {params})
+        const asyncRes = await axios(`${url}weather?zip=${params.zipcode}&units=imperial&appid=${params.key}`)
         const value = asyncRes.data;
         setData(value);
         setLoading(false)
@@ -39,11 +39,16 @@ function App() {
     return <div className="App">Loading...</div>
   }
 
+  // const dayWeather = data.forecast.forecastday
+  // console.log(dayWeather)
 
+  // dayWeather.map((res) => {
+  //   console.log(res.astro.sunrise)
+  // })
   return (
     <div className="App">
       <h1>Weather App!</h1>
-      <p>{data.location.name}</p>
+      <h2>{data.name}</h2>
     </div>
   );
 }
