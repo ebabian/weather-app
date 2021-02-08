18e6f3af371c64e453cb184adedfd1feb098c5a6
ebabian <babianelektra@gmail.com>
2021-02-07 20:14:03 -0500

    search a city name and render 5 days of weather data

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import axios from 'axios';
 
 function App() {
 
-  const [data, setData] = useState([])
-  const [isLoading, setLoading] = useState(true)
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [city, setCity] = useState('');
 
   const API_KEY = process.env.REACT_APP_API_KEY;
 
@@ -17,39 +18,43 @@ function App() {
 
   const params = {
       key: API_KEY,
-      zipcode: '11238'
+      days: 5
   }
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const asyncRes = await axios(`${url}weather?zip=${params.zipcode}&units=imperial&appid=${params.key}`)
-        const value = asyncRes.data;
-        setData(value);
-        setLoading(false)
-        console.log(value)
-      } catch(err) {
-        console.log(err)
-      }
+  const searchData = async (event) => {
+    if(event.key === "Enter") {
+      await fetch(
+        `${url}forecast?q=${city}&units=imperial&cnt=${params.days}&appid=${params.key}`
+      )
+        .then((res) => res.json())
+        .then((resData) => {
+          console.log(resData)
+          setData(resData)
+          setLoading(false)
+        })
     }
-    fetchData();
-  }, [])
-
-  if(isLoading) {
-    return <div className="App">Loading...</div>
+    // if(isLoading) {
+    //   return <div className="data">Loading...</div>
+    // }
   }
 
-  // const dayWeather = data.forecast.forecastday
-  // console.log(dayWeather)
-
-  // dayWeather.map((res) => {
-  //   console.log(res.astro.sunrise)
-  // })
   return (
+    
     <div className="App">
       <h1>Weather App!</h1>
+      <input
+        type="text"
+        placeholder="Search a city..."
+        className="search"
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+        onKeyPress={searchData}
+      />
+      <div className="data">
       <h2>{data.name}</h2>
+      </div>
     </div>
+  
   );
 }
 
